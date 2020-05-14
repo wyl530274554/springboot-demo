165c70e16376887da12836e28526cce90edc7d62
Melon <530274554@qq.com>
2020-05-14 23:41:31 +0800

    in tomcat

--- src/main/java/com/melon/apk/ApkApplication.java
+++ src/main/java/com/melon/apk/ApkApplication.java
@@ -3,13 +3,24 @@ package com.melon.apk;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * 入口
+ *
+ * 若要将项目部署到tomcat中，需要继承至SpringBootServletInitializer
+ * 否则找不到入口
+ */
 @MapperScan("com.melon.apk.mapper")
 @SpringBootApplication
-public class ApkApplication {
+public class ApkApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApkApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApkApplication.class, args);
     }
-
 }
