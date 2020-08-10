73b96ae5b561e9962685050534324658d8b0cc22
Melon <530274554@qq.com>
2020-08-11 00:22:28 +0800

    上传apk

--- src/main/java/com/melon/apk/interceptor/InterceptorConfig.java
+++ src/main/java/com/melon/apk/interceptor/InterceptorConfig.java
@@ -10,15 +10,17 @@ import java.util.List;
 
 /**
  * 拦截器管理
+ *
+ * 这个配置会让spring boot的自动配置失效
  */
-@Configuration
-public class InterceptorConfig extends WebMvcConfigurationSupport {
-    /**
-     * 处理controller直接返回string会乱码的问题
-     */
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new StringHttpMessageConverter(StandardCharsets.UTF_8));
-        addDefaultHttpMessageConverters(converters);
-    }
-}
+//@Configuration
+//public class InterceptorConfig extends WebMvcConfigurationSupport {
+//    /**
+//     * 处理controller直接返回string会乱码的问题
+//     */
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        converters.add(new StringHttpMessageConverter(StandardCharsets.UTF_8));
+//        addDefaultHttpMessageConverters(converters);
+//    }
+//}
